efb0fff8a36eacb2eaca561ec24106770e38dd0d
Marcondes F Pinheiro <bshoopkss@gmail.com>
2022-07-14 20:40:36 -0300

    secondCommit

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,14 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { loadPosts } from "../../contexts/PostsProvider/actions";
 import { PostsContext } from "../../contexts/PostsProvider/context";
 
 export const Posts = () => {
   const postsContext = useContext(PostsContext);
-  const {postsState, postsDispatch} = postsContext;
-  console.log(postsState)
+  const { postsState, postsDispatch } = postsContext;
+
+  useEffect(() => {
+    loadPosts(postsDispatch);
+  }, [postsDispatch]);
 
   return (
     <div>
       <h1>POSTS</h1>
+      {postsState.loading && (
+        <p>
+          <strong>Carregando...</strong>
+        </p>
+      )}
+
+      {postsState.posts.map((p) => (
+        <p key={p.id}>{p.title}</p>
+      ))}
     </div>
   );
 };
--- src/contexts/PostsProvider/actions.js
+++ src/contexts/PostsProvider/actions.js
@@ -1,8 +1,11 @@
-import * as types from './types';
+import * as types from "./types";
 
-export const loadPosts = async () => {
-  const postsRaw = 
-  await fetch('https://jsonplaceholder.typicode.com/posts')
-  const posts = await postsRaw.json();
-  dispatch({ type: types.POSTS_SUCESS, payload: posts })
-}
+export const loadPosts = async (dispatch) => {
+  dispatch({ type: types.POSTS_LOADING });
+  
+  setTimeout(async () => {
+    const postsRaw = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await postsRaw.json();
+    dispatch({ type: types.POSTS_SUCESS, payload: posts });
+  }, 5000);
+};
